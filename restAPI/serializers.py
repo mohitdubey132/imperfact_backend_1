0127548e11a88f43f26dfb30e7fb2f6110487798
mohitdubey132 <mohitdubey1322001@gmail.com>
2023-12-09 12:01:14 +0530

    add new featuser in serializer

--- restAPI/serializers.py
+++ restAPI/serializers.py
@@ -4,14 +4,14 @@ from .models import CustomUser ,Question , Answers
 class CustomUserSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = CustomUser
-		fields =['U_id', 'userType','password','userName','fullName', 'age', 'gender', 'address', 'mobileNo', 'country']
+		fields =['U_id', 'userType','userName','fullName', 'age', 'gender', 'address', 'mobileNo', 'country']
         
-# class QuestionSerializer(serializers.ModelSerializer):
-# 	class Meta:
-# 		model = Question
-# 		fields =['Q_id','title','description','pub_date','updated_date','like_count','dislike_count']
-# 	# Make pub_date optional
-#     extra_kwargs = {'pub_date': {'required': False}}
+#-----------------for use in other Serializer --------------------------------------------------# 
+class CustomUtilsUserSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = CustomUser
+		fields =[ 'userType','userName','fullName']
+
 
 class QuestionSerializer(serializers.ModelSerializer):
     class Meta:
@@ -25,11 +25,12 @@ class QuestionSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         # The 'user' field is already included in the validated_data
         return Question.objects.create(**validated_data)
-
+# ------------------------------------------answer Serializer ------------------------------------------------
 class AnswersSerializer(serializers.ModelSerializer):
+    user = CustomUtilsUserSerializer(source='user_id', read_only=True)
     class Meta:
         model = Answers
-        fields = ['Answer','pub_date','updated_date','user_id','Q_id','like_count','dislike_count','A_id']
+        fields = ['Answer','pub_date','updated_date','user_id','Q_id','like_count','dislike_count','A_id','user']
     extra_kwargs = {'pub_date': {'required': False},
 	'user_id': {'required': False}
 	}   
