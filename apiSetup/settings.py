dcb0b0f74c4e55024ef599e0bfa7b5e9225a56a4
ReactjswebztechieMD <reactjs.webztechie@gmail.com>
2023-12-17 16:33:31 +0530

    postgres setup done

--- apiSetup/settings.py
+++ apiSetup/settings.py
@@ -94,10 +94,21 @@ WSGI_APPLICATION = 'apiSetup.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'PostgresMD',
+        'HOST': 'database-1.cutkqxamclk2.ap-south-1.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
